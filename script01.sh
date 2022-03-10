0028799571ddcc7d34765f29fcbeadb272c7d191
Sithum Nimlaka Abeydheera <79376462+Sithum-Nimlaka@users.noreply.github.com>
2022-03-10 13:25:27 +0530

    Fix Failed to download metadata for repo

--- script01.sh
+++ script01.sh
@@ -1,3 +1,9 @@
+#Fix Failed to download metadata for repo
+#CentOS Linux 8 had reached the End Of Life (EOL) on December 31st, 2021.
+#It means that CentOS 8 will no longer receive development resources from the official CentOS project. 
+#After Dec 31st, 2021, if you need to update your CentOS, you need to change the mirrors to vault.centos.org where they will be archived permanently.
+#Alternatively, you may want to upgrade to CentOS Stream.
+
 echo "####################################################"
 echo "##### Fix Failed to download metadata for repo #####"
 echo "####################################################"
